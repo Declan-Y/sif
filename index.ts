7dc91d2ec486b80bb7e57edbe34b2fe4a38e7cba
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2023-07-01 23:07:26 +1000

    begin adding auth

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import expressSession from "express-session";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: "https://localhost:3000",
+  clientID: "B8BtoZkwNhUf1qcl0IiLMoFFKYt3YsYZ",
+  issuerBaseURL: "https://dev-5x0s2u4w2onpnsxq.us.auth0.com",
+};
+app.use(auth(config));
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
 
 app.listen(port, () => {
